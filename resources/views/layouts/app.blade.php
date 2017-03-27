716d3fe28e2b0eda7586d5d9d75e8a9560122282
eladio-02 <eladio-02@users.noreply.github.com>
2017-03-27 15:32:32 -0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,8 +50,8 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Ingresar</a></li>
-                            <li><a href="{{ url('/register') }}">Registro</a></li>
+                            <!-- <li><a href="{{ url('/login') }}">Ingresar</a></li>
+                            <li><a href="{{ url('/register') }}">Registro</a></li> -->
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
