4a2f4ed01f5166df395326871f2272f5d8402f06
rafaqueal <rafa.quesada.al@hotmail.com>
2017-04-04 10:08:48 -0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Sistema Mejoramiento Génetico</div>
+                <div class="panel-heading">Programa de Mejoramiento Génetico</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
